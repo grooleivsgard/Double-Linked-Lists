64382e10ad82d071010b23c4256f454ed7e52ade
gro97 <gro.oleivsgard@gmail.com>
2021-10-05 11:01:53 +0200

    The method LeggInn(T verdi) produces the correct output. The method toString() is not working correctly.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -17,16 +17,18 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     public static void main(String[] args) {
 
         //// Oppgave 1 ///////////////////////////////////////////////////
-        Liste<String> liste = new DobbeltLenketListe<>();
-        System.out.println(liste.antall() + "" + liste.tom());
-        // Utskrift: 0 true
 
-        String[] s = {"Ole", null, "Per", "Kari", null};
-        Liste<String> stringListe = new DobbeltLenketListe<>(s);
-        System.out.println("Med null-verdier: " + stringListe.antall() + "" + stringListe.tom());
-        System.out.println(stringListe);
-        // Utskrift: 3 false
-
+        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.toString() + " " + liste.omvendtString());
+        for (int i = 1; i <= 3; i++) {
+            liste.leggInn(i);
+            System.out.println(liste.toString() + " " + liste.omvendtString());
+        }
+// Utskrift:
+// [] []
+// [1] [1]
+// [1, 2] [2, 1]
+// [1, 2, 3] [3, 2, 1]
 
 
     }
